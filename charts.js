8268a9739ce7a5e7f92f29e3bc3e94d154f96531
unknown <drctcarter@gmail.com>
2022-03-10 13:43:05 -0500

    Updating charts.js

--- charts.js
+++ charts.js
@@ -42,14 +42,7 @@ function buildMetadata(sample) {
 
     // Use `.html("") to clear any existing metadata
     PANEL.html("");
-    PANEL.append("h6").text(result.id);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.bbtype);
-    PANEL.append("h6").text(result.wfreq);
-
+    
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
@@ -64,40 +57,67 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    var samples = [];
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-    var filterSamples = samples.filter()
+    var filterSamples = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
-    var samplesResult = samplesFirst[0];
+    var samplesResult = filterSamples[0];
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-    d3.json("samples.json").then(function(data){
-      otu_ids = data.samples.map(persons => persons.otu_ids);
-     });
-
-     d3.json("samples.json").then(function(data){
-      otu_labels = data.samples.map(persons => persons.otu_labels);
-     });
-
-     d3.json("samples.json").then(function(data){
-      sample_values = data.samples.map(persons => persons.sample_values);
-     });
+    
+    otu_ids = samplesResult.otu_ids;
+         
+    otu_labels = samplesResult.otu_labels;
+      
+    sample_values = samplesResult.sample_values;
+     
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
+    
+    //d3.json("samples.json").then(function(data){
+    otu_ids_sorted = data.samples.map(persons => persons.otu_ids).sort((a,b) => b.length - a.length);
+    console.log(otu_ids_sorted);
+//});
 
-    var yticks = 
+    var yticks = otu_ids_sorted.slice(0,10).reverse();
+
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var trace = {
+      x: otu_labels,
+      y: yticks_rev,
+      type: "bar",
+      orientation: "h"
+    };
+    
+    var barData = [trace];
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found",
+      xaxis: {title: "Sample Values"},
+      yaxis: {title: "OTU-ID's"}
+
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("plotArea", barData, barLayout);
   });
 }
+//d3.json("samples.json").then(function(data){
+ // otu_ids_sorted = data.samples.map(persons => persons.otu_ids).sort((a,b) => a.length - b.length);
+ // console.log(otu_ids_sorted);
+//});
+
+
+//var trace1 = {
+ // x: data.map(row => row.greekSearchResults),
+ // y: data.map(row => row.greekName),
+ // text: data.map(row => row.greekName),
+ // name: "Greek",
+ // type: "bar",
+ // orientation: "h"
+//};
+
+
